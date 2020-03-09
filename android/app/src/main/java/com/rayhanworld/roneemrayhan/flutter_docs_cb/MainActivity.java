1e375ab1c401792546e03bba6e3f653b1d573ea4
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2020-03-09 09:18:05 +0600

    0=> Doc API image_picker async io file camera etc...

---
+++ android/app/src/main/java/com/rayhanworld/roneemrayhan/flutter_docs_cb/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rayhanworld.roneemrayhan.flutter_docs_cb;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
